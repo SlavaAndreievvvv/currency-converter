c1704a5598d5d30cfb3d765d73a8d2e1b4f1aa54
ANDREIEV VIACHESLAV <andreiev.viacheslav@gmail.com>
2023-06-02 11:42:18 +0300

    ⚡️ add hooks useLoading

---
+++ utils/hooks/useLoading.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export const useLoading = (initialState = false) => {
+  const [loading, setLoading] = useState(initialState);
+
+  const startLoading = () => {
+    setLoading(true);
+  };
+
+  const stopLoading = () => {
+    setLoading(false);
+  };
+
+  return [loading, startLoading, stopLoading];
+};
